932e1abe899901ae12ee778728d0035adc2b7c52
MsHapen <otaviobentto@gmail.com>
2023-03-24 16:55:02 -0300

    livros

--- livraria/models.py
+++ livraria/models.py
@@ -25,3 +25,12 @@ class Autor(models.Model):
     
     class Meta:
         verbose_name_plural = "Autores"
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField(default=0)
+    preço = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+
+    def __str__(self):
+        return f"Livro{self.titulo} ({self.quantidade})"
